ee98f6d895f3cc90407d306d9cc7e0d4355533e3
AngelFloresMrDarkSoul <60407543+AngelFloresMrDarkSoul@users.noreply.github.com>
2020-03-11 11:51:53 -0800

    terminado

---
+++ Actores/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Actores
+{
+    
+    class actores
+    {
+        private string nom;
+        public actores(string n)
+        {
+            nom = n;
+        }
+        public void imprime()
+        {
+            Console.WriteLine(nom);
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<actores> Actor = new List<actores>();
+            Actor.Add(new actores("Will Smith"));
+            Actor.Add(new actores("Dwayne Johnson"));
+            Actor.Add(new actores("Zac Efron"));
+            Actor.Add(new actores("Robert Downey Jr."));
+ 
+            foreach(actores a in Actor)
+            {
+                a.imprime();
+            }
+        }
+    }
+}
