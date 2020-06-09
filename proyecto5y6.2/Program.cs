4eb672e9bc7d65207466f1d001b3c8f4e3d739af
AngelFloresMrDarkSoul <60407543+AngelFloresMrDarkSoul@users.noreply.github.com>
2020-06-09 01:07:27 -0700

    nuevo intento

---
+++ proyecto5y6.2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace proyecto5y6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
